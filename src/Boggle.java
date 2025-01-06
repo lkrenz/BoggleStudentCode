0d4370b8d0a3e17bf0c002007ddcd7abc6829e4d
lkrenz <liamkrenz100@gmail.com>
2025-01-06 13:42:03 -0800

    Outlined code and started designing method architecture.

--- src/Boggle.java
+++ src/Boggle.java
@@ -10,10 +10,22 @@ public class Boggle {
         // TODO: Complete the function findWords(). Add all words that are found both on the board
         //  and in the dictionary.
 
+        // Convert the dictionary into a TST
+
+        // Use DFS to search through the 2D array
+        // In the DFS keep track of the path taken through and back up once a null value is found on the TST
+        // Could maybe use a Trie here, much simpler and wouldn't be massive because of small alphabet
+        // Everytime a true value is encountered in the Trie / tst simply remove the word from the TST (set it to false)
+
+
         // Convert the list into a sorted array of strings, then return the array.
         String[] sol = new String[goodWords.size()];
         goodWords.toArray(sol);
         Arrays.sort(sol);
         return sol;
+    }
+
+    public static String[] depthFirstSearch() {
+        return null;
     }
 }
