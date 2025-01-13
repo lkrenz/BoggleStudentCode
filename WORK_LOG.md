af20b1620279c1bea1e22ac18876d1d96403ce0e
lkrenz <liamkrenz100@gmail.com>
2025-01-12 16:49:37 -0800

    Finished

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,4 +1,4 @@
-Name: YOUR NAME HERE
+Name: Liam Krenz
 
 | Date       |    Time     |                                                                                                                                                                                                                                          Update |
 |:-----------|:-----------:|------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
@@ -7,6 +7,7 @@ Name: YOUR NAME HERE
 | January 8  | 11:25-11:55 |                                                                                        I spent this time thinking out the depth first solution. During htis time I attained a very strong conceptual undersatnding and began to implement code. |
 | January 10 | 9:10-10:10  | I spent this time implementing the code as I outlined it in my comments. The code is very close to working, but there are ways in which I can speed it up. I need some way of preening branches off of the tree once they are no longer needed. |
 | January 12 |  4:30-4:41  |                                                                                    I think I caught the error, I was using an old method that creates a new child node instead of returning null. I need to update my test cases to be certain. |
+| January 12 |  4:41-4:47  |                                                                                                                                                              Updated test files and everything works correctly. Checked comments and formating. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Boggle.java
+++ src/Boggle.java
@@ -7,27 +7,21 @@ public class Boggle {
 
         ArrayList<String> goodWords = new ArrayList<String>();
 
-        // TODO: Complete the function findWords(). Add all words that are found both on the board
-        //  and in the dictionary.
-
         // Convert the dictionary into a Trie
         Trie dict = new Trie();
 
-        for (int i = 0; i < dictionary.length; i++) {
-            dict.addWord(dictionary[i]);
+        for (String s : dictionary) {
+            dict.addWord(s);
         }
 
         // Use DFS to search through the 2D array
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[i].length; j++) {
+
+                // Initiate depth first recursive search at every letter
                 depthFirstSearch(board, dict.getRoot(), goodWords, i, j, "");
             }
         }
-
-        // In the DFS keep track of the path taken through and back up once a null value is found on the TST
-        // Could maybe use a Trie here, much simpler and wouldn't be massive because of small alphabet
-        // Everytime a true value is encountered in the Trie / tst simply remove the word from the TST (set it to false)
-
 
         // Convert the list into a sorted array of strings, then return the array.
         String[] sol = new String[goodWords.size()];
@@ -38,6 +32,7 @@ public class Boggle {
 
     // Uses depth first search to search through inputted 2D array
     public static void depthFirstSearch(char[][] board, TrieNode place, ArrayList<String> goodWords, int row, int col, String path) {
+
         // Base cases are outside of bounds
         if (row >= board.length || row < 0 || col < 0 || col >= board[row].length) {
             return;
@@ -47,6 +42,8 @@ public class Boggle {
         if (board[row][col] == '-') {
             return;
         }
+
+        // Update node
         place = place.checkChild(board[row][col]);
 
         // null value in the trie
@@ -63,6 +60,8 @@ public class Boggle {
         // Check if there's a word
         if (place.isWord()) {
             goodWords.add(newPath);
+
+            // Everytime a true value is encountered in the Trie / tst simply set the node to false
             place.removeWord();
         }
 
@@ -71,6 +70,7 @@ public class Boggle {
         depthFirstSearch(board, place, goodWords, row + 1, col, newPath);
         depthFirstSearch(board, place, goodWords, row, col - 1, newPath);
         depthFirstSearch(board, place, goodWords, row - 1, col, newPath);
+
         // Set this character back to the original character
         board[row][col] = letter;
     }
